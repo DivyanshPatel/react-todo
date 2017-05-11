5778fbfe038132f06bbe41bad0f0a2871dd4fd51
DivyanshPatel <nealbay@gmail.com>
2017-05-11 16:54:54 +0530

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,19 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+// Load foundation
+
+//$(document).foundation();
+
+// App styles
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+
+  <p1>React BoilerPlate</p1>,
+
+document.getElementById('app')
+);
---
+++ readme.md
@@ -0,0 +1,1 @@
+BoilerPlated React
